82408d222e7e9e962757b8b871f16ca01f703d6a
Sara Subtil <subtilsara026@gmail.com>
2021-02-16 14:10:50 +0000

    finished

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+
+  def index
+      @tasks = Task.all
+    end
+
+    def new
+      @task = Task.new
+    end
+
+    def create
+      @task = Task.new(params_task)
+      @task.save
+      redirect_to tasks_path
+    end
+
+    def edit
+      @task = Task.find(params[:id])
+    end
+
+    def update
+      @task = Task.find(params[:id])
+      @task.update(params_task)
+      redirect_to task_path(@task)
+    end
+
+    def show
+      @task = Task.find(params[:id])
+    end
+
+    def destroy
+      @task = Task.find(params[:id])
+      @task.destroy
+      redirect_to tasks_path(@tasks)
+    end
+
+    private
+
+    def params_task
+      params.require(:task).permit(:title, :details, :completed)
+    end
+end
+
+
+
+
+
+
